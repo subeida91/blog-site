1347bfba5926475b5f517223d98a8ae5c7cda6b2
Subeida <subeida91@gmail.com>
2021-01-07 22:29:14 +0000

    Added initial posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,8 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'pages',
-        path: '${__dirname}/src/pages'
+        path: `${__dirname}/src/pages/`,
+
       },
     },
 
@@ -14,15 +15,15 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
-        path: '${__dirname}/src/posts'
-      },
+        path: `${__dirname}/src/posts/`,     
+       },
     },
 
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: '${__dirname}/src/images'
+        path: `${__dirname}/src/images/`,
       },
     },
     `gatsby-plugin-sharp`, 
