d241b8e9d208970df49ab36bac1d51e8aeb1a066
Subeida <subeida91@gmail.com>
2021-01-07 22:55:46 +0000

    added style components

---
+++ src/themes/theme.js
@@ -0,0 +1,39 @@
+export default {
+    fonts: {
+        main: 'limelight, source sans pro',
+        code: "Roboto Mono, monospace",
+    },
+
+    colors: {
+        main1: "hs1(207,70%,59%)",
+        main2: "hs1(207,70%,94%)",
+        dark1: "hs1(227,2%,12%)",
+        dark2: "hs1(227,2%,26%)",
+        dark3: "hs1(227,2%,64%)",
+        light1: "hs1(0,0%,97%)",
+        light2: "hs1(0,0%,99%)",
+    },
+
+    breakpoints: {
+        mobile: "only screen and (max-width: 50rem)",
+        tablet: "only screen and (max-width: 65rem)",
+    },
+
+    spacings: {
+        xxSmall: ".25rem",
+        xSmall: ".5rem",
+        small: "1rem",
+        medium: "2rem",
+        large: "3rem",
+        xLarge: "4rem",
+        xxLarge: "6rem",
+    },
+
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link: "color 0.2s ease",
+    },
+    shadows: {
+        shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+    },
+}
