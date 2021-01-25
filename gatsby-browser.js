af34d099362a255d1c1c6980a1d5e56f9576c8a7
Subeida <subeida91@gmail.com>
2021-01-25 14:36:06 +0000

    code styling with prism

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { createGlobalStyle, ThemeProvider} from "styled-components"
+import {MDXProvider} from "@mdx-js/react"
+import {preToCodeBlock} from "mdx-utils"
 import Theme from "./src/themes/theme"
+import {Table, Code} from "./src/components"
+import "./language-tabs.css"
 
 const GlobalStyles = createGlobalStyle`
 *{
@@ -14,9 +18,25 @@ body, html {
     background-color: ${props => props.theme.colors.light1};
 }
 `
+
+const components = {
+    table: Table,
+    pre: preProps => {
+        const props = preToCodeBlock(preProps)
+
+        if (props) {
+            return <Code {...props} />
+        }
+        return <pre {...preProps} />
+        },
+    wrapper: ({ children }) => <>{children}</>,
+}
+
 export const wrapRootElement = ({ element }) => (
+    <MDXProvider components ={components}>
     <ThemeProvider theme= {Theme}>
         <GlobalStyles />
         {element}
     </ThemeProvider>
+    </MDXProvider>
 )
